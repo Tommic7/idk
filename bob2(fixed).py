7d38937dcd371754e7491c4076a510cbca27ddd8
Tommic7 <nick.resseler@gmail.com>
2025-01-08 13:41:37 +0100

    Update bob2(fixed).py
    its fixed

--- bob2(fixed).py
+++ bob2(fixed).py
@@ -1,9 +1,10 @@
 import time
+
 time.sleep(3)
-Print('Hello')
+print('Hello')
 time.sleep(1)
 print('I am a program that is purely made for testing with Python')
-time.wait(2)
+time.sleep(2)
 print('I hope i am helpfull to you in a good way')
 time.sleep(2)
 print('And i hope someguy has coded in some stuff in here')
